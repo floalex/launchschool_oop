5fb6f9f48f8df971bfd7cbb41700586a1aaa38b2
floalex <flo.liang@my.com>
2016-04-20 06:13:26 +0000

    Complete todo list methods

--- todolist/todo.rb
+++ todolist/todo.rb
@@ -80,6 +80,10 @@ class TodoList
     todos.pop
   end
   
+  def done?
+    todos.all? { |todo| todo.done? }
+  end
+  
   def remove_at(index)
     raise IndexError if index > todos.size
     todos.delete_at(index)
@@ -90,23 +94,48 @@ class TodoList
     text << @todos.map(&:to_s).join("\n")
     text
   end
+  
+  def each 
+    todos.each do |todo|
+      yield(todo)
+    end
+    self
+  end
+  
+  def select
+    list = TodoList.new(title)
+    each do |todo|
+      list.add(todo) if yield(todo)
+    end
+    list
+  end
+  
+  def find_by_title(title)
+    select { |todo| todo.title == title }.first
+  end
+  
+  def all_done
+    select { |todo| todo.done? }
+  end
+  
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+  
+  def mark_done(title)
+    find_by_title(title) && find_by_title(title).done!
+  end
+  
+  def mark_all_done
+    each do |todo|
+      todo.done!
+    end
+  end
+  
+  def mark_all_undone
+    each do |todo|
+      todo.undone!
+    end
+  end
 end
 
-todo1 = Todo.new("Buy milk")
-todo2 = Todo.new("Clean room")
-todo3 = Todo.new("Go to gym")
-
-list = TodoList.new("Today's Todos")
-list.add(todo1)
-list.add(todo2)
-list.add(todo3)
-
-puts list
-
-list.pop
-
-puts list
-
-list.mark_done_at(1)
-
-puts list
